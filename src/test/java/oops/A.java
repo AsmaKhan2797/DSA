1366d976ed655b05e64a44a50df626b1bc5c5b8c
AsmaKhan2797 <asmakhan1427@gmail.com>
2020-03-18 15:23:39 +0530

    primitive data type example

--- src/test/java/oops/A.java
+++ src/test/java/oops/A.java
@@ -1,13 +1,36 @@
 package test.java.oops;
 
-public class _super_ {
-	 int a =10;
-	 static int b=100;
-	 public void m1() {
-		 this.a=12;
-		 this.b=11;
-		 System.out.println("a :"+this.a);
-		 System.out.println("b :"+this.b);
-	 }
-	
+public class A {
+	int a = 10;
+
+	public void m1(int a) {
+		this.a = a;
+
+		System.out.println("a :" + this.a);
 	}
+
+	 class B  {
+		 int a =444;
+		B() {
+			//super();
+
+			//System.out.println("b :" + this.a);
+		}
+
+		void show() {
+			System.out.println("b :" + this.a);
+		}
+	}
+}
+class AAA{
+
+	public static void main(String[] args) {
+		
+		//this is not allowed in static context
+		//this.a=22;
+		A a = new A();
+		A.B bb = a.new B();
+		bb.show();
+	
+}
+}
--- src/test/java/oops/_this_.java
+++ src/test/java/oops/_this_.java
@@ -1,17 +1,17 @@
 package test.java.oops;
 
-public class _this_ extends _super_{
+public class _this_ extends A{
 	public void test_super() {
 		super.a=123;
-		super.b=0X09;
+		
 	}
 	//super.a=100;
 	public static void main(String[] args) {
-		_super_ sp= new _super_();
+		_this_ sp= new _this_();
 		//this is not allowed in static context
 		//this.a=22;
 		
-		sp.m1();
+		sp.m1(4);
 
 	}
 	 
